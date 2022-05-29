7a701ca51fb168f656c7246e18298710dac32bf9
Anton Khozyainov <a.khozyainov@cherryhome.ai>
2022-05-29 21:33:21 +0300

    hw 5.2

--- cache.go
+++ cache.go
@@ -3,21 +3,43 @@ package cache
 import "time"
 
 type Cache struct {
+	data map[string]record
+}
+
+type record struct {
+	value    string
+	deadline time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{data: map[string]record{}}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	r, ok := c.data[key]
+	if !r.deadline.IsZero() && r.deadline.Before(time.Now()) {
+		delete(c.data, key)
+		return "", false
+	}
+	return r.value, ok
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.data[key] = record{value: value}
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	var keys []string
+	now := time.Now()
+	for k, r := range c.data {
+		if !r.deadline.IsZero() && r.deadline.Before(now) {
+			continue
+		}
+		keys = append(keys, k)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.data[key] = record{value: value, deadline: deadline}
 }
